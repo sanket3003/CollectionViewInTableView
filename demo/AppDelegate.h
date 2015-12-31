a697d51a7003aa242ee0ca10bcfe33827a4758f1
Sanket Bhavsar <sanket.b@BalaBaskar.local>
2015-12-31 16:04:09 +0530

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Sanket Bhavsar on 12/31/15.
+//  Copyright (c) 2015 Sanket Bhavsar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo
+//
+//  Created by Sanket Bhavsar on 12/31/15.
+//  Copyright (c) 2015 Sanket Bhavsar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
